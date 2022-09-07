61c1b986e71f37481b965ade8db9bf399efd5fb9
Selsanat <leandro.benites@edu.devinci.fr>
2022-09-07 15:27:54 +0200

    Tourne Cam

--- Assets/Script/Leandro/PlayerMoovementThirdPerson.cs
+++ Assets/Script/Leandro/PlayerMoovementThirdPerson.cs
@@ -12,6 +12,7 @@ public class PlayerMoovementThirdPerson : MonoBehaviour
   public float speed = 6f;
 
   public float turnSmoothTime = 0.1f;
+  public Quaternion sheesh = Quaternion.Euler(0f,0f,0f);
 
   float turnSmoothVelocity;
     // Start is called before the first frame update
@@ -23,17 +24,27 @@ public class PlayerMoovementThirdPerson : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      if (Input.GetKey("l")){
+        sheesh = Quaternion.Euler(sheesh.eulerAngles.x,sheesh.eulerAngles.y-0.3f,sheesh.eulerAngles.z);
+      }
+      if (Input.GetKey("r")){
+        sheesh = Quaternion.Euler(sheesh.eulerAngles.x,sheesh.eulerAngles.y+0.3f,sheesh.eulerAngles.z);
+      }
+      if (Input.GetKey("o")){
+        sheesh = Quaternion.Euler(perso.rotation.eulerAngles.x,perso.rotation.eulerAngles.y,perso.rotation.eulerAngles.z);
+      }
       float horizontal = Input.GetAxisRaw("Horizontal");
       float vertical = Input.GetAxisRaw("Vertical");
       Vector3 direction = new Vector3(horizontal,0f,vertical).normalized;
 
       if (direction.magnitude >= 0.1f){
 
-        float targetAngle = Mathf.Atan2(direction.x, direction.z)* Mathf.Rad2Deg;
+        float targetAngle = Mathf.Atan2(direction.x, direction.z)* Mathf.Rad2Deg+cam.eulerAngles.y;
         float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
         transform.rotation= Quaternion.Euler(0f,angle,0f);
-        controller.Move(direction*speed*Time.deltaTime);
-        point.rotation= Quaternion.Euler(0f,0f,0f);
+        Vector3 moveDir = Quaternion.Euler(0f,targetAngle,0f)*Vector3.forward;
+        controller.Move(moveDir.normalized*speed*Time.deltaTime);
       }
+      point.rotation= sheesh;
     }
 }
