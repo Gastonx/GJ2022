bd4aec2b59297ff58ecfc5860d2de471363c5ed7
Gastonx <56830127+Gastonx@users.noreply.github.com>
2022-09-09 09:03:40 +0100

    Attack

--- Assets/Script/Hugo/Attack.cs
+++ Assets/Script/Hugo/Attack.cs
@@ -4,25 +4,12 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    public BoxCollider attackTrigger;
-
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            attackTrigger.enabled = true;
-        }
-        
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        Debug.Log("Stay");
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enter");
+        if (other.tag== "Ennemie")
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
